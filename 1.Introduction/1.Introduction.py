f1b649594f33f568a2aabcdd65a23b9bc697b57a
buildscript-dev <buildscrip.dev@gmail.com>
2025-09-08 15:00:40 +0530

    Initializing the project

---
+++ 1.Introduction/1.Introduction.py
@@ -0,0 +1,13 @@
+
+print("Hello World")
+print("Hello Python")
+print("Hello Madara")
+print("Hello BuildScript")
+print("Welcome to first program")
+
+
+print("My name is Python")
+print("My name is Build...", "BuildScript")
+print("Python is 50+ year old.")
+print("BuildScript is", 10+10, "years old")
+
---
+++ 1.Introduction/2.Variable.py
@@ -0,0 +1,17 @@
+
+
+# A variable is a name give to a memory location in a program.
+# Variable is container which stores data...
+#like name = "BuildScript"
+#     age = 20
+#     salary = 12.5 cr
+# (=) is Assignment operator
+
+
+name = "BuildScript"
+age = 20
+salary = 12.345
+
+print(name)
+print(age)
+print(name + " have salary of",salary,"cr")
