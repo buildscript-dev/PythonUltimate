ad3e154808241023aae31bf80a84c3f6aa7a0150
buildscript-dev <buildscrip.dev@gmail.com>
2025-09-10 15:22:14 +0530

    UserInput

--- 1.Introduction/4.TypeCasting.py
+++ 1.Introduction/4.TypeCasting.py
@@ -1,4 +1,5 @@
 
+# Int -> String
 a = 123 # Int
 b = "456" # String
 
@@ -7,7 +8,7 @@ b = "456" # String
 print(a)
 print(type(a))
 
-# Now we need to assign the value of the a to string, So,
+# Now we need to assign the value of a to string, So,
 # We need to make a new variable:
 c = str(a)
 # Here the str tells it's converting into string and (a) is value which is converting into..
@@ -16,4 +17,13 @@ print(c)
 print(type(c))
 # So, The output we receive is str....
 
+# Float -> String
+d = 3.14
+print(d)
+print(type(d))
 
+# So, we do it in similar way,
+
+e = str(d)
+print(e)
+print(type(e))
---
+++ 1.Introduction/5.UserInput.py
@@ -0,0 +1,28 @@
+
+# To Take UserInput we can use,
+# Input(): Input Function, by default it return string...
+
+name = input("Enter your name: ")
+# Oooh, It takes input but haven't returned anything?
+# So, we need to use print() for that,
+
+# print(name)
+# So, It's print my name but it don't look good
+# Then use this to look good.
+print("Hello,", name)
+
+# Next if we want to do addition,
+# firstNum = input("Enter you firstNum: ")
+# secondNum = input("Enter you secondNum: ")
+
+# print(firstNum + secondNum)
+# Fuck, the output is not correct,
+# Yes, Because it is by default string, So convert it into int or float,
+
+# 1. Way
+# firstNum = int(firstNum)
+# secondNum = int(secondNum)
+# 2. Way
+firstNum = int(input("Enter your firstNum: "))
+secondNum = int(input("Enter your secondNum: "))
+print( firstNum + secondNum)
