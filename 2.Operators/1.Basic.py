1a01957bc21fbee71834cbbd7fe80194ba1bace3
buildscript-dev <buildscrip.dev@gmail.com>
2025-09-13 13:10:47 +0530

    Updating Operation

---
+++ 2.Operators/1.Basic.py
@@ -0,0 +1,3 @@
+# Operators are symbols that perform operations on variables and values.
+# Python has several types of operators for different tasks like:
+# arithmetic, comparision, logical operations, and more
---
+++ 2.Operators/2.Arithmetic.py
@@ -0,0 +1,34 @@
+# Total 7 arithmetic operators
+# like +-*/%,//,pow()
+
+first_Num = int(input("Enter your first number: "))
+second_Num = int(input("Enter your second number: "))
+
+# Addtion
+print(f"The Sum of Both Values are {first_Num + second_Num}")
+
+# Subtraction
+print(f"The Subtraction of Both Values are {first_Num - second_Num}")
+
+# Multiplication
+print(f"The Multiplication of Both Values are {first_Num * second_Num}")
+
+# Division
+print(f"The Division of Both Values are {first_Num / second_Num}")
+# Here we can found the value to be in float
+# To Avoid the we use floor division... 
+
+# Floor Divison 
+print(f"The Floor Division of Both Values are {first_Num // second_Num}")
+
+# Exponential
+third_Num = int(input("Enter a  number: "))
+exponent_value = int(input("Enter Exponent Values: "))
+print(f"Simple Exponentent {third_Num**2}")
+print(f"Simple Exponentent {third_Num**3}")
+print(f"The Subtraction of Both Values are {third_Num**exponent_value}")
+
+# Modulus - Remainder Values
+print(f"The Subtraction of Both Values are {first_Num % second_Num}")
+
+# BODMAS -Bracket of division multiplicaiton  additon subration
